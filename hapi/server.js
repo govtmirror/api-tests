0ccf41abff397a1332c3686fa6b71b9c3aec1454
Hursh Prasad <hurshprasad@gmail.com>
2014-12-24 19:09:49 -0500

    added uncaught exception

--- hapi/server.js
+++ hapi/server.js
@@ -11,6 +11,10 @@ server.connection({
 server.route({ method: 'GET', path: '/proceedings', handler: proceedings.all });
 server.route({ method: 'GET', path: '/proceedings/{name}', handler: proceedings.one });
 
+process.on('uncaughtException', function (err) {
+    console.log(err);
+});
+
 // Start the server
 server.start(function () {
     console.log('Server running at:', server.info.uri);
